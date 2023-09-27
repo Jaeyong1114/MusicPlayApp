2281b4239a3b36e603a35943d59f0f5632121b53
Jaeyong Kim <ajfnwodyd3@naver.com>
2023-09-27 15:03:40 +0900

    Mediaplayer

--- app/src/main/java/com/example/musicplayapp/MainActivity.kt
+++ app/src/main/java/com/example/musicplayapp/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.example.musicplayapp
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.musicplayapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    private var mediaPlayer : MediaPlayer? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.playButton.setOnClickListener{
+            mediaPlayerPlay()
+        }
+        binding.pauseButton.setOnClickListener{
+            mediaPlayerPause()
+        }
+        binding.stopButton.setOnClickListener{
+            mediaPlayerStop()
+        }
+    }
+
+
+    private fun mediaPlayerPlay(){
+        if(mediaPlayer == null){
+            mediaPlayer = MediaPlayer.create(this, R.raw.music1).apply {
+                isLooping=true
+            }
+
+        }
+        mediaPlayer?.start()
+    }
+
+    private fun mediaPlayerPause(){
+        mediaPlayer?.pause()
+    }
+
+    private fun mediaPlayerStop(){
+        mediaPlayer?.stop()
+        mediaPlayer?.release()
+        mediaPlayer  = null
     }
 }
