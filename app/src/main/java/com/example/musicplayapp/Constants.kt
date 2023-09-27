19f2648c3bfa116c786fe0b81be1957b5ff91061
Jaeyong Kim <ajfnwodyd3@naver.com>
2023-09-27 15:42:01 +0900

    startService

---
+++ app/src/main/java/com/example/musicplayapp/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.musicplayapp
+
+const val MEDIA_PLAYER_PLAY = "play"
+const val MEDIA_PLAYER_PAUSE = "pause"
+const val MEDIA_PLAYER_STOP = "stop"
+
--- app/src/main/java/com/example/musicplayapp/MainActivity.kt
+++ app/src/main/java/com/example/musicplayapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.musicplayapp
 
+import android.content.Intent
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,7 +8,6 @@ import com.example.musicplayapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
-    private var mediaPlayer : MediaPlayer? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityMainBinding.inflate(layoutInflater)
@@ -26,22 +26,22 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun mediaPlayerPlay(){
-        if(mediaPlayer == null){
-            mediaPlayer = MediaPlayer.create(this, R.raw.music1).apply {
-                isLooping=true
-            }
 
-        }
-        mediaPlayer?.start()
+        val intent = Intent(this, MediaPlayerSerivce::class.java)
+            .apply{action = MEDIA_PLAYER_PLAY}
+        startService(intent)
+
     }
 
     private fun mediaPlayerPause(){
-        mediaPlayer?.pause()
+        val intent = Intent(this, MediaPlayerSerivce::class.java)
+            .apply{action = MEDIA_PLAYER_PAUSE}
+        startService(intent)
     }
 
     private fun mediaPlayerStop(){
-        mediaPlayer?.stop()
-        mediaPlayer?.release()
-        mediaPlayer  = null
+        val intent = Intent(this, MediaPlayerSerivce::class.java)
+            .apply{action = MEDIA_PLAYER_STOP}
+        startService(intent)
     }
 }
